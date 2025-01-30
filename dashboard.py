24cf1eceb9381bf64f2ce7210b59317c69772e8c
Qwandy <sarunas.slepetis@hotmail.com>
2025-01-30 15:18:11 +0000

    Adds location search feature to within application gui.

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,11 @@ load_dotenv()
 class gui():
     
     def __init__(self):
-        pass
+        self.state = 0
+        self.weather = None
+        self.temp = None 
+        self.humidity = None 
+        self.wind = None
 
     def _fetch_info(self, location):
 
@@ -33,104 +37,125 @@ class gui():
             print("Error", response.status_code)
         return data_weather, data_main_temp, data_main_humidity, data_wind
     
+    def submit_location(self):
+        loc = self.location_textbox.get()
+        self.weather, self.temp, self.humidity, self.wind = self._fetch_info(loc)
+        self.state = 1
+        self.run_gui()
+        
+
     def run_gui(self):
     
         root = Tk()
         root.title('Weather Flash')
 
-        # mainframe
-        mainframe = ttk.Frame(root, padding = "3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky = (N, W, E, S))
-        root.columnconfigure(0, weight = 1)
-        root.rowconfigure(0, weight = 1)
-
-        # logic dictating what weather image to display
-        weather, temp, humidity, wind = self._fetch_info("Didcot")
-        if weather == "Fog":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/fog.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Snow":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/snow.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Lightning":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/lightning.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Rain":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/rain.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Clear":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clear.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Mist":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/mist.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        elif weather == "Clouds":
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clouds.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
-        else:
-            image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clear.jpeg").resize((30,30))
-            photo = ImageTk.PhotoImage(image)
-
-            lbl = Label(mainframe, image = photo)
-            lbl.image = photo
-            lbl.grid(column=0, row=0)
+        if self.state == 0:
             
-        # weather widget
-        ttk.Label(mainframe, text = f"The weather is currently: {weather}").grid(column=1, row=0, sticky=W)
+            location_label = Label(root, text = "Enter a location")
+            location_label.grid(column=0, row=0, sticky = W)
 
-        # temperature widget
-        ttk.Label(mainframe, text = f"The temperature is currently: {temp:.02f}°C").grid(column=1, row=1, sticky=W)
-        image_thermo = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/thermo.jpeg").resize((30,30))
-        photo_thermo = ImageTk.PhotoImage(image_thermo)
-        lbl_thermo = Label(mainframe, image = photo_thermo)
-        lbl_thermo.image = photo
-        lbl_thermo.grid(column=0,row=1)
+            self.location_textbox = Entry(root)
+            self.location_textbox.grid(column=0, row=1, sticky = W)
 
-        # humidity widget
-        ttk.Label(mainframe, text = f"The humidity is currently: {humidity}%").grid(column=1, row=2, sticky=W)
-        image_water = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/water.jpeg").resize((30,30))
-        photo_water = ImageTk.PhotoImage(image_water)
-        lbl_water = Label(mainframe, image = photo_water)
-        lbl_water.image = photo
-        lbl_water.grid(column=0,row=2)
+            location_button = Button(root, text = "Submit", command = self.submit_location)
+            location_button.grid(column = 0, row = 2, sticky = W)
 
-        # wind widget
-        ttk.Label(mainframe, text = f"The wind speed is currently: {wind} km/h").grid(column=1, row=3, sticky=W)
-        image_wind = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/wind.jpeg").resize((30,30))
-        photo_wind = ImageTk.PhotoImage(image_wind)
-        lbl_wind = Label(mainframe, image = photo_wind)
-        lbl_wind.image = photo
-        lbl_wind.grid(column=0, row=3)
+            root.mainloop()
+        elif self.state == 1:
 
-        root.mainloop()
+            # mainframe
+            mainframe = ttk.Frame(root, padding = "3 3 12 12")
+            mainframe.grid(column=0, row=0, sticky = (N, W, E, S))
+            root.columnconfigure(0, weight = 1)
+            root.rowconfigure(0, weight = 1)
+
+            # logic dictating what weather image to display
+            
+            if self.weather == "Fog":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/fog.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Snow":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/snow.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Lightning":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/lightning.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Rain":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/rain.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Clear":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clear.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Mist":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/mist.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            elif self.weather == "Clouds":
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clouds.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+            else:
+                image = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/clear.jpeg").resize((30,30))
+                photo = ImageTk.PhotoImage(image, master = mainframe)
+
+                lbl = Label(mainframe, image = photo)
+                lbl.image = photo
+                lbl.grid(column=0, row=0)
+                
+            # weather widget
+            ttk.Label(mainframe, text = f"The weather is currently: {self.weather}").grid(column=1, row=0, sticky=W)
+
+            # temperature widget
+            ttk.Label(mainframe, text = f"The temperature is currently: {self.temp:.02f}°C").grid(column=1, row=1, sticky=W)
+            image_thermo = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/thermo.jpeg").resize((30,30))
+            photo_thermo = ImageTk.PhotoImage(image_thermo, master = mainframe)
+            lbl_thermo = Label(mainframe, image = photo_thermo)
+            lbl_thermo.image = photo
+            lbl_thermo.grid(column=0,row=1)
+
+            # humidity widget
+            ttk.Label(mainframe, text = f"The humidity is currently: {self.humidity}%").grid(column=1, row=2, sticky=W)
+            image_water = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/water.jpeg").resize((30,30))
+            photo_water = ImageTk.PhotoImage(image_water, master = mainframe)
+            lbl_water = Label(mainframe, image = photo_water)
+            lbl_water.image = photo
+            lbl_water.grid(column=0,row=2)
+
+            # wind widget
+            ttk.Label(mainframe, text = f"The wind speed is currently: {self.wind} km/h").grid(column=1, row=3, sticky=W)
+            image_wind = Image.open("/home/qwandy/CodeProjects/Code/meteor_dashboard/weather_icons/wind.jpeg").resize((30,30))
+            photo_wind = ImageTk.PhotoImage(image_wind, master = mainframe)
+            lbl_wind = Label(mainframe, image = photo_wind)
+            lbl_wind.image = photo
+            lbl_wind.grid(column=0, row=3)
+
+            root.mainloop()
 
 
 test = gui().run_gui()
